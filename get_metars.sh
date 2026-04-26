6c6ba11986993629f824f3bfb7a411be0cb997f2
steve <emailstevekim@gmail.com>
2026-04-26 17:49:19 -0400

    Update

--- get_metars.sh
+++ get_metars.sh
@@ -4,4 +4,4 @@
 curl "https://aviationweather.gov/data/cache/metars.cache.xml.gz" | gunzip -c > data/metars/metars.cache.xml
 
 ## tafs
-# https://aviationweather.gov/data/cache/tafs.cache.xml.gz | gunzip -c > data/metars/tafs.cache.xml
+curl https://aviationweather.gov/data/cache/tafs.cache.xml.gz | gunzip -c > data/metars/tafs.cache.xml
