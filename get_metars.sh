e9938b823d40e27f0a6e18c4537dbbfef00659f9
steve <emailstevekim@gmail.com>
2026-04-27 22:59:23 -0400

    Update

--- get_metars.sh
+++ get_metars.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 ## download metars
-curl "https://aviationweather.gov/data/cache/metars.cache.xml.gz" | gunzip -c > data/metars/metars.cache.xml
-
+curl "https://aviationweather.gov/data/cache/metars.cache.csv" > data/metars/metars.csv
 ## tafs
-curl https://aviationweather.gov/data/cache/tafs.cache.xml.gz | gunzip -c > data/metars/tafs.cache.xml
+#curl https://aviationweather.gov/data/cache/tafs.cache.xml | gunzip -c > data/metars/tafs.csv
+
+ogr2ogr data/metars/metars.gpkg data/metars/metars.vrt
+
+## join to airports_large
+rm -rf data/metars/metars.geojson
+ogr2ogr -f GeoJSON data/metars/metars.geojson -dialect SQLITE -sql "SELECT m.*, p.municipality, p.continent, p.iso_country, p.iso_region, p.wikipedia_link FROM airports_large p INNER JOIN metars m ON m.station_id = p.icao_code" data/metars/metars_airports.vrt
+
+rm -rf data/metars/metars.gpkg
